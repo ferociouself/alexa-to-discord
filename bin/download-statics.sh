958d58a9a06845bd0b46ab7f4f0c26025f6e1f45
Jackson Ekis <jhe3cd@virginia.edu>
2019-01-12 20:50:28 -0800

    Recursive S3

--- bin/download-statics.sh
+++ bin/download-statics.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd $HOME/alexa-to-discord
 
-aws s3 cp s3://discord-bot-secret/ ./
-aws s3 cp s3://alexa-to-discord-sounds/ ./sounds/
+aws s3 cp s3://discord-bot-secret/ ./ --recursive
+aws s3 cp s3://alexa-to-discord-sounds/ ./sounds/ --recursive
