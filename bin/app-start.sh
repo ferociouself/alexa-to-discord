a412b62eac4436398561afbc9483724d8ae6ab84
Jackson Ekis <jhe3cd@virginia.edu>
2019-01-14 16:03:48 -0800

    Moving forever install time

--- bin/app-start.sh
+++ bin/app-start.sh
@@ -7,4 +7,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm use --delete-prefix v10.15.0
+npm install forever -g
 forever start ./forever.json -l combined.log
